d2c6377cc836a38ca56fbe31a8fc8f6ea3d7bea5
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-17 14:22:32 +0100

    Implement user registration functionality with JSON input handling

--- frontendreg.php
+++ frontendreg.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello, World!";
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$json = file_get_contents("php://input");
+
+$userDetails = json_decode($json);
+
+// echo json_encode([$userDetails]);
+
+$first_name = $userDetails['firstName'];
+$last_name = $userDetails['lastName'];
+$email = $userDetails['email'];
+$hashed = password_hash($userDetails['password'], PASSWORD_DEFAULT);
+
+$query = "INSERT INTO `users`(`first_name`, `last_name`, `email`, `hashed`) VALUES ('$first_name', '$last_name', '$email', '$hashed')";
+
 ?>
