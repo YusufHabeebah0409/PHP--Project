0729585dc2849c9c9f7886127a641b1c3c27029d
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-23 14:12:05 +0100

    Refactor login functionality to include JWT token generation and update user token in database

--- config.php
+++ config.php
@@ -4,4 +4,6 @@
  header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$secretKey = '9726af86fb03779b8b5fc78cbfe0992622a85b05b6d3cdaff081ccad8a2c795c';
 ?>
--- frontendlogin.php
+++ frontendlogin.php
@@ -2,7 +2,7 @@
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-require('autoload.php');
+require('vendor/autoload.php');
 
 require('config.php');
 $loginDetails = json_decode(file_get_contents("php://input"));
@@ -10,13 +10,31 @@ $loginDetails = json_decode(file_get_contents("php://input"));
 $email = $loginDetails->email;
 $password = $loginDetails->password;
 
+// echo bin2hex(random_bytes(32));
+
 $query = "SELECT * FROM `users` WHERE `email`='$email'";
 $result = mysqli_query($connection, $query);
 
 if (mysqli_num_rows($result) > 0) {
     $user = mysqli_fetch_assoc($result);
     if (password_verify($password, $user['password'])) {
-        echo json_encode(['status' => true, 'message' => 'Login Successful']);
+        $payLoad = [
+            'user_id' => $user['user_id'],
+            'email' => $user['email'],
+            'role' => 'user',
+            'iat' => time(),
+            'exp' => time() + 3600
+        ];
+        $token = JWT:: encode($payLoad, $secretKey, 'HS256');
+        $id = $user['user_id'];
+        $updateQuery = "UPDATE user SET token='$token' WHERE user_id=$id";
+        $check = mysqli_query($connection, $updateQuery);
+        if($check){
+        echo json_encode(['status' => true, 'message' => 'Login Successful', 'token' => $token]);
+        }else{
+            echo json_encode(['status' => false, 'message' => 'Login failed']);
+        }
+
     } else {
         echo json_encode(['status' => false, 'message' => 'Invalid Password']);
     }
